3d2c5af16eef35dd0555475b3583576d16371516
MadCrack <doors-95@live.com.mx>
2016-11-18 17:07:44 -0600

    Update validar_movimiento_torre.c
    Agregue algo al parecer estaba vacio

--- validar_movimiento_torre.c
+++ validar_movimiento_torre.c
@@ -1,1 +1,19 @@
 /*Se esta esperando actualización*/
+int validar_movimiento_torre (int pos_ini_x,int pos_ini_y,int pos_x,int pos_y,char tablero[8][8])
+{
+
+
+    if(pos_ini_x==pos_x && pos_ini_y!=pos_y)
+    {
+        return 0;
+    }
+    if(pos_ini_y==pos_y && pos_ini_x!=pos_x)
+    {
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
